b48e752df0ce6fa994003d5c75a3675b73baa965
S Purushotham Shreyas Reddy <purushotham.shreyas2023@vitstudent.ac.in>
2026-08-24 02:30:01 +0530

    Codes updates 3

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import re
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/static"
+)
 
 
 # =========================================================
